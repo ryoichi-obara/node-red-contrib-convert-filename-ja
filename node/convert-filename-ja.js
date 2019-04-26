8d88131c2ad1c9a0e681be3b4fde29b17093df3c
ryoichi-obara <ryoichi-obara@users.noreply.github.com>
2019-04-27 00:09:15 +0900

    Initial commit.

---
+++ node/convert-filename-ja.js
@@ -0,0 +1,31 @@
+const util = require('convert-filename-ja');
+
+/**
+ * exportsするfunctionの定義.
+ * @see https://nodered.jp/docs/creating-nodes/node-js
+ * @param {object} RED - Node-REDで決められているお作法.
+ */
+function exportsFunction(RED) {
+  /**
+   * registerTypeで登録するfunctionの定義.
+   * @see https://nodered.jp/docs/creating-nodes/node-js#ノードコンストラクタ
+   * @param {object} config
+   */
+  function nodeFunction(config) {
+    const thisNode = this;
+    RED.nodes.createNode(thisNode, config);
+
+    // inputイベント(ノードがメッセージを受信)へのリスナー登録.
+    this.on('input', async (msg) => {
+      const payload = util.convert(msg.payload);
+      msg.payload = payload;
+      thisNode.send(msg);
+    });
+  }
+
+  RED.nodes.registerType('convert-filename-ja', nodeFunction, {
+    // credentials
+  });
+}
+
+module.exports = exportsFunction;
